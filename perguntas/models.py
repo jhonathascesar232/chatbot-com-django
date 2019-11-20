b303578f82027ea04964f6e9f13d94cdf98305d0
Jhonathas César <jhonathascesar232@gmail.com>
2019-11-20 14:10:45 -0300

    adicionando o modelo na aplicacao perguntas

--- perguntas/models.py
+++ perguntas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Captura(models.Model):
+	code = models.CharField(max_length=15)
+	code_user = models.CharField(max_length=15)
+	active = models.IntegerField()
+	code_ralation = models.CharField(max_length=15) # irá relacio a resposta atual com a pergunta anterior
+	question = models.CharField(max_length=500)		# pergunta
+	answer = models.CharField(max_length=500)		# resposta
+
+	def __str__(self):
+		return self.question
