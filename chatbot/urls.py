2b7b266ba171ba7dc071b183f9f95583b0a5f2fc
Jhonathas César <jhonathascesar232@gmail.com>
2019-11-21 04:51:08 -0300

    finalizando as migrações

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-	path('', include('home.urls')),
-	path('capturas/', include('capturas.urls')),
-	path('home/', include('home.urls')),
-	path('perguntas/', include('perguntas.urls')),
-	path('usuarios/', include('usuarios.urls')),
+	# path('', include('home.urls')),
+	# path('capturas/', include('capturas.urls')),
+	# path('home/', include('home.urls')),
+	# path('perguntas/', include('perguntas.urls')),
+	# path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ]
