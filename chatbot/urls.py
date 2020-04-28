3d87fa1ace4ece03b1842ff2457f5a12a55cbacc
Jhonathas César <jhonathascesar232@gmail.com>
2020-04-28 18:11:21 -0300

    Criação da virtualenv

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-	# path('', include('home.urls')),
+	path('', include('home.urls')),
 	# path('capturas/', include('capturas.urls')),
 	# path('home/', include('home.urls')),
 	# path('perguntas/', include('perguntas.urls')),
